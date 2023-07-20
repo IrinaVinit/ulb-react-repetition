e00058e155051838c5ff1615b882050554c475d8
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-07-20 19:02:34 +0300

    feat: add AppRouter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from "react";
 import "./app.css";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import About from "./pages/About";
-import Posts from "./pages/Posts";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/UI/navbar/Navbar";
+import AppRouter from "./components/AppRouter";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <div className="navbar">
-      <div className="navbar-items">
-        <Link to='/about'>О сайте</Link>
-        <Link to='/posts'>Посты</Link>
-      </div>
-    </div>
-      <Routes>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/posts" element={<Posts />} />
-      </Routes>
+    <Navbar/>
+      <AppRouter/>
     </BrowserRouter>
   );
 }

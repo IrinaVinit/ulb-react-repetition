15875498d04de8a0c3cfe28d1b5fb57d8ee6c224
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-07-20 18:50:44 +0300

    feat: add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./app.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <div className="navbar">
+      <div className="navbar-items">
+        <Link to='/about'>О сайте</Link>
+        <Link to='/posts'>Посты</Link>
+      </div>
+    </div>
       <Routes>
         <Route path="/about" element={<About/>}/>
         <Route path="/posts" element={<Posts />} />

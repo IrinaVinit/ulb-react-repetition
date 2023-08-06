297ef5849ad21be91c129b521feed6273b76c544
Irina Vinitskaia <kreneva.i@yandex.ru>
2023-08-06 19:22:39 +0300

    feat: add useObserver

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -2,24 +2,23 @@ import { useMemo, useState } from "react";
 // кастомные пользовательские хуки - это хуки, которые внутри себя испльзуют стандартные реакт-хуки
 
 export const useSortedPosts = (posts, sort) => {
-      // отсортированные посты
+  // отсортированные посты
   const sortedPosts = useMemo(() => {
-    console.log("Отработала");
     if (sort) {
       return [...posts].sort((a, b) => a[sort].localeCompare(b[sort]));
     }
 
     return posts;
-  }, [sort, posts])
+  }, [sort, posts]);
   return sortedPosts;
-}
+};
 
 export const usePosts = (posts, sort, query) => {
-    const sortedPosts = useSortedPosts(posts, sort)
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter((post) =>
-          post[sort]?.toLowerCase().includes(query.toLowerCase())
-        );
-      }, [query, sortedPosts]);
-    return sortedAndSearchedPosts;
-}
+  const sortedPosts = useSortedPosts(posts, sort);
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter((post) =>
+      post[sort]?.toLowerCase().includes(query.toLowerCase())
+    );
+  }, [query, sortedPosts]);
+  return sortedAndSearchedPosts;
+};
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 // import "./app.css";
-import PostList from "../components/PostList"
+import PostList from "../components/PostList";
 import PostForm from "../components/PostForm";
 import PostFilter from "../components/PostFilter";
 import MyModal from "../components/UI/modal/MyModal";
@@ -11,78 +11,80 @@ import Loader from "../components/UI/loader/Loader";
 import { useFetching } from "../hooks/useFetching";
 import { getPagesArray, getPagesCount } from "../utils/pages";
 import Pagination from "../components/UI/pagination/Pagination";
+import { useObserver } from "../hooks/useObserver";
 
 const Posts = () => {
-    //  посты с сервера
-    const [posts, setPosts] = useState([]);
-    const [filter, setFilter] = useState({ sort: "", query: "" });
-    const [modal, setModal] = useState(false);
-    const [limit, setLimit] = useState(10);
-    const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(0);
-  
-    const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
-      const response = await PostService.getAll(limit, page);
-      setPosts(response.data);
-      const totalCount = response.headers["x-total-count"];
-      setTotalPages(getPagesCount(totalCount, limit));
-    });
+  //  посты с сервера
+  const [posts, setPosts] = useState([]);
+  const [filter, setFilter] = useState({ sort: "", query: "" });
+  const [modal, setModal] = useState(false);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const lastElement = useRef();
     
-   
-    useEffect(() => {
-      fetchPosts(limit, page);
-    }, []);
-  
-    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  
-    const createPost = (newPost) => {
-      setPosts([...posts, newPost]);
-      setModal(false);
-    };
-    // получаем пост из дочернего компонента
-    const removePost = (post) => {
-      setPosts(posts.filter((p) => p.id !== post.id));
-    };
-  
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
+    const response = await PostService.getAll(limit, page);
+    setPosts([...posts, ...response.data]);
+    const totalCount = response.headers["x-total-count"];
+    setTotalPages(getPagesCount(totalCount, limit));
+  });
+
+  useObserver(lastElement, page < totalPages, isPostsLoading, () => {setPage(page + 1)})
+
+  useEffect(() => {
+    fetchPosts(limit, page);
+  }, [page, limit]);
+
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+    setModal(false);
+  };
+  // получаем пост из дочернего компонента
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
   const changePage = (page) => {
-  setPage(page);
-  fetchPosts(limit, page);
-  }
-  
-    return (
-      <div className="App">
-        <button onClick={fetchPosts}>Get posts</button>
-        <MyButton
-          style={{ marginTop: 30 }}
-          onClick={() => {
-            setModal(true);
-          }}
-        >
-          Создать пост
-        </MyButton>
-        <MyModal visible={modal} setVisible={setModal}>
-          <PostForm createCb={createPost} />
-        </MyModal>
-        <hr style={{ margin: "15px" }}></hr>
-        <PostFilter filter={filter} setFilter={setFilter} />
-  
-        {postError && <h1>Произошла ошибка {postError}</h1>}
-  
-        {isPostsLoading ? (
-          <div style={{ display: "flex", justifyContent: "center", marginTop: 50 }}>
-            <Loader />
-          </div>
-        ) : (
-          <PostList
-            removeCb={removePost}
-            posts={sortedAndSearchedPosts.length ? sortedAndSearchedPosts : posts}
-            title="Список постов 1"
-          />
-        )}
-        <Pagination page={page} changePageCb={changePage} totalPages={totalPages}/>
-      </div>
-    );
-  }
-  
+    setPage(page);
+   
+  };
+
+  return (
+    <div className="App">
+      <button onClick={fetchPosts}>Get posts</button>
+      <MyButton
+        style={{ marginTop: 30 }}
+        onClick={() => {
+          setModal(true);
+        }}
+      >
+        Создать пост
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm createCb={createPost} />
+      </MyModal>
+      <hr style={{ margin: "15px" }}></hr>
+      <PostFilter filter={filter} setFilter={setFilter} />
+
+      {postError && <h1>Произошла ошибка {postError}</h1>}
+      <PostList
+        removeCb={removePost}
+        posts={sortedAndSearchedPosts.length ? sortedAndSearchedPosts : posts}
+        title="Список постов 1"
+      />
+      <div ref={lastElement} style={{ height: 20, background: "red" }} />
+      {isPostsLoading && (
+        <div style={{ display: "flex", justifyContent: "center", marginTop: 50 }}>
+          <Loader />
+        </div>
+      )}
+      <Pagination page={page} changePageCb={changePage} totalPages={totalPages} />
+    </div>
+  );
+};
 
 export default Posts;
